11e2eb033eb7d774949fb8ad3cb84900b03144a7
Neal Stephenson <neal@bakerst.org>
2015-02-28 14:12:26 -0500

    Initial checkin of basic data Notes: Basic description of robot Orangutan/Makefile: Standard Makefile for Orangutan PI/robot.py: Shell of python code

---
+++ PI/robot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+
+from serial import Serial
+from re import compile
+from RPIO import Setup, add_interrupt_callback, IN
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RobRoy
+
+This is basic control libraries for robot with Orangutan SVP-1284
+Board and a RPI higher control center.
